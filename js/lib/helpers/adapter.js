7d379e1937d94c4bb2667895dcf6ca36ff9d3e43
Ruy Adorno <ruyadorno@hotmail.com>
2014-10-25 10:53:41 -0400

    Adapter cleanup

--- js/lib/helpers/adapter.js
+++ js/lib/helpers/adapter.js
@@ -2,16 +2,6 @@
 
 var logger = require('yeoman-environment/lib/util/log');
 
-function GUIPrompt(q) {
-    this.answer = null;
-    this.question = q;
-}
-
-GUIPrompt.prototype.run = function (cb) {
-    // we should get the answer from the front-end before calling cb
-    cb(this.answer || this.question.default);
-};
-
 function GUIAdapter() {
 
     function promptModule(questions, allDone) {
@@ -19,26 +9,7 @@ function GUIAdapter() {
         allDone({});
     }
 
-    promptModule.prompts = {
-        list: {},
-        input: {},
-        confirm: {},
-        rawlist: {},
-        expand: {},
-        checkbox: {},
-        password: {}
-    };
-
-    promptModule.registerPrompt = function (name, Prompt) {
-        this.prompts[name] = Prompt;
-        return this;
-    };
-
     this.prompt = promptModule;
-
-    Object.keys(this.prompt.prompts).forEach(function (promptName) {
-        this.prompt.registerPrompt(promptName, GUIPrompt);
-    }, this);
 
     this.diff = function () {};
     this.log = logger();
