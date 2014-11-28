dec99439c34311411bede15669e39493049e07bb
Ruy Adorno <ruyadorno@hotmail.com>
2014-11-28 02:08:42 -0500

    Styled grid items using material-ui elements

--- app/scripts/components/grid.jsx
+++ app/scripts/components/grid.jsx
@@ -1,15 +1,52 @@
 'use strict';
 
+var _ = require('lodash');
 var React = require('react');
+var Paper = require('material-ui/src/js/paper.jsx');
+var materialColors = require('material-colors/dist/colors.json');
 
 var GeneratorStore = require('../stores/generator-store');
 var GridActions = require('../actions/grid-actions');
 
 
+// Stores an array with all material colors to be used on grid render
+var colors = (function getAvailableColors(fromColors) {
+
+  var arr = [];
+
+  Object.keys(fromColors).forEach(function (key) {
+    Object.keys(fromColors[key]).forEach(function (colorKey) {
+      arr.push(fromColors[key][colorKey]);
+    });
+  });
+
+  return arr;
+})(materialColors);
+
+
 var GridItem = React.createClass({
+
+  getInitialState: function() {
+    return {
+      color: _.sample(colors),
+      zDepth: 1
+    };
+  },
 
   _onClick: function () {
     GridActions.gridItemSelected(this.props.name);
+  },
+
+  _onMouseOver: function() {
+    this.setState({
+      zDepth: 4
+    });
+  },
+
+  _onMouseOut: function() {
+    this.setState({
+      zDepth: 1
+    });
   },
 
   render: function () {
@@ -17,9 +54,19 @@ var GridItem = React.createClass({
     var filename = 'img/' + this.props.name + '.png';
 
     return (
-      <figure onClick={this._onClick}>
-        <img src={filename} />
-      </figure>
+      <Paper
+        className="grid-item"
+        zDepth={this.state.zDepth}
+        onMouseOver={this._onMouseOver}
+        onMouseOut={this._onMouseOut}
+        onClick={this._onClick}>
+        <div className="grid-bg" style={{ backgroundColor: this.state.color }}></div>
+        <figure
+          className="grid-img"
+          style={{backgroundImage: 'url(' + filename + ')'}}>
+        </figure>
+        <h3 className="grid-item-heading">{this.props.name}</h3>
+      </Paper>
     );
   }
 
