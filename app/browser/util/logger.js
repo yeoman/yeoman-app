6186d7a3e91788f010e2e70187690187b34ace64
Stefan Buck <github@stefanbuck.com>
2014-12-05 01:12:18 +0100

    add logging library

---
+++ app/browser/util/logger.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var app = require('app');
+var bunyan = require('bunyan');
+var PrettyStream = require('bunyan-prettystream');
+var argv = require('minimist')(process.argv);
+
+// Bunyan is a simple and fast JSON logging library.
+// Please take a closer look to the readme and sample
+//  to see how Bunyan works and how powerful it is.
+// https://github.com/trentm/node-bunyan
+
+// A "stream" is Bunyan's name for an output for log messages.
+// https://github.com/trentm/node-bunyan#streams
+var streams = [];
+
+// Pipe the logs in a file. The file is located in the
+// applications configuration directory.
+var logPath = path.join(app.getDataPath(), 'yoapp.log');
+streams.push({
+  level: argv.loglevel || bunyan.INFO,
+  path: logPath
+});
+
+// Clean up log file.
+// TODO: Implement rotating file
+// https://github.com/trentm/node-bunyan#stream-type-rotating-file
+if (fs.existsSync(logPath)) {
+  fs.writeFileSync(logPath, '');
+}
+
+// Pipe the logs to the console.
+// Start the app with the `--debug` argument to
+// enable the debug mode.
+if (argv.debug) {
+
+  // Prettify the stream for the console
+  var prettyStdOut = new PrettyStream({
+    useColor: true
+  });
+  prettyStdOut.pipe(process.stdout);
+
+  streams.push({
+    level: argv.loglevel || bunyan.TRACE,
+    type: 'raw',
+    stream: prettyStdOut
+  });
+}
+
+var log = bunyan.createLogger({
+
+  // Give the logger the app name.
+  name: app.getName(),
+
+  // Log the source file, line and function name.
+  // Therfore you should use named function
+  // https://github.com/trentm/node-bunyan#src
+  src: true,
+
+  // Add the outputsgst
+  streams: streams
+});
+
+// Advanced logging for our devs. Log logger errors.
+if (argv.debug) {
+  log.on('error', function (err) {
+      console.warn('An error in bunyan occurred:', err);
+  });
+}
+
+// Print some  information about the system and the application.
+log.info('Run %s in version %s', app.getName(), app.getVersion());
+log.info('Platform: %s', process.platform);
+log.info(process.versions, 'Versions:');
+
+module.exports = log;
--- app/index.js
+++ app/index.js
@@ -1,19 +1,20 @@
 'use strict';
 
 var app = require('app');
-var debug = require('debug')('yoapp:main');
+var YoApplication = require('./browser/yo-application');
+var log = require('./browser/util/logger');
 
-process.on('uncaughtException', function(error) {
-  console.app(error.message);
-  console.app(error.stack);
+
+// Catch unhandled exceptions
+process.on('uncaughtException', function uncaughtException(error) {
+  log.error(error);
 });
 
-app.on('will-finish-launching', function () {
+app.on('will-finish-launching', function willFinishLaunching() {
   setupCrashReporter();
 });
 
 function setupCrashReporter() {
-  debug('setupCrashReporter()');
   // TODO: Register own crash reporter server
   // https://github.com/atom/atom-shell/blob/master/docs/api/crash-reporter.md
   require('crash-reporter').start();
@@ -21,8 +22,8 @@ function setupCrashReporter() {
 
 // This method will be called when atom-shell has done everything
 // initialization and ready for creating browser windows.
-app.on('ready', function () {
-  debug('Event: ready');
-  var YoApplication = require('./browser/yo-application.js');
-  YoApplication.ope2n();
+app.on('ready', function ready() {
+  log.trace('Start application');
+
+  YoApplication.open();
 });
