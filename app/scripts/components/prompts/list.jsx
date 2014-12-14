f4d8d1bdf3adf1b3d581cf02ced5f765bc44009b
Ruy Adorno <ruyadorno@hotmail.com>
2014-12-14 01:44:02 -0500

    Added material-ui list prompt

--- app/scripts/components/prompts/list.jsx
+++ app/scripts/components/prompts/list.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react');
+var RadioButton = require('material-ui/src/js/radio-button.jsx');
 
 var PromptMixin = require('./prompt-mixin');
 
@@ -15,10 +16,16 @@ var ListPrompt = React.createClass({
     };
   },
 
+  _getRefName: function (name, value) {
+    return 'list-item-' + name + '-' + value;
+  },
+
   _onChange: function (event) {
+    /*
     this.setState({
       answer: event.currentTarget.value
     });
+    */
   },
 
   render: function () {
@@ -26,26 +33,31 @@ var ListPrompt = React.createClass({
     var choices = this.props.choices.map(function (choice, index) {
 
       var def = this.props.defaultAnswer;
-      var checked = def === index || def === choice.value;
+      var value = choice.value || choice;
+      var name = choice.name || choice;
+      var ref = this._getRefName(name, value);
+      var checked = def === index || def === value;
 
       return (
-        <span className="list">
-          <label htmlFor={this.props.name}>{choice.name}</label>
-          <input
-            type="radio"
-            name={this.props.name}
-            value={choice.value}
-            checked={checked}
-            onChange={this._onChange}
-          />
-        </span>
+        <RadioButton
+          key={ref}
+          ref={ref}
+          name={this.props.name}
+          value={value}
+          label={name}
+          checked={checked}
+          onChange={this._onChange}
+          className="list-prompt-list-item"
+        />
       );
-    });
+    }, this);
 
     return (
-      <fieldset>
+      <fieldset className="list-prompt">
         <label style={{ background: this.props.color }}>{this.props.message}</label>
-        {choices}
+        <div className="list-prompt-list">
+          {choices}
+        </div>
       </fieldset>
     );
   }
