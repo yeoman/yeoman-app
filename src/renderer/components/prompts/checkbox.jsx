bac89d00faf8520dae48e60977c8ce6365c60144
Ruy Adorno <ruyadorno@hotmail.com>
2015-06-21 13:52:03 -0400

    Updated to use new checkbox material-ui component

--- src/renderer/components/prompts/checkbox.jsx
+++ src/renderer/components/prompts/checkbox.jsx
@@ -8,50 +8,6 @@ var Checkbox = mui.Checkbox;
 var PromptMixin = require('./prompt-mixin');
 
 
-var CheckboxPromptItem = React.createClass({
-
-  getInitialState: function () {
-    return {
-      checked: this.props.defaultChecked
-    };
-  },
-
-  _onChange: function (event) {
-    this.setState({
-      checked: this.refs[this._getRefName()].state.checked
-    });
-
-    this.props.onChange();
-    event.preventDefault();
-  },
-
-  _getRefName: function () {
-    return 'checkbox-item-' + this.props.name + '-' + this.props.value;
-  },
-
-  render: function () {
-    var checkboxStyle = {
-      width: 'auto'
-    };
-
-    return (
-      <div className="checkbox-prompt-item">
-        <Checkbox
-          type="checkbox"
-          ref={this._getRefName()}
-          name={this.props.name}
-          value={this.props.value}
-          checked={this.state.checked}
-          onChange={this._onChange}
-          style={checkboxStyle}
-        />
-        <label htmlFor={this.props.name}>{this.props.name}</label>
-      </div>
-    );
-  }
-});
-
-
 var CheckboxPrompt = React.createClass({
 
   mixins: [PromptMixin],
@@ -97,13 +53,15 @@ var CheckboxPrompt = React.createClass({
         this.props.defaultAnswer.indexOf(choice.value) > -1;
 
       return (
-        <CheckboxPromptItem
+        <Checkbox
+          type="checkbox"
           key={ref}
           ref={ref}
           name={choice.name}
+          label={choice.name}
           value={choice.value}
           defaultChecked={checked}
-          onChange={this._onChange}
+          style={{ marginBottom: 10 }}
         />
       );
 
@@ -112,7 +70,7 @@ var CheckboxPrompt = React.createClass({
     return (
       <div className="checkbox-prompt fieldset">
         <label style={{ background: this.props.color }}>{this.props.message}</label>
-        <div className="checkbox-prompt-list">
+        <div className="checkbox-prompt-list" style={{ margin: 20 }}>
           {choices}
         </div>
       </div>
