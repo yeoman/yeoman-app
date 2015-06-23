0ce754609715ba0e8e4ba6e0b22e3ee0bceb35da
Stefan Buck <github@stefanbuck.com>
2015-06-24 00:48:00 +0200

    Fix #31 by striping colors from array

--- src/renderer/utils/colors.js
+++ src/renderer/utils/colors.js
@@ -5,8 +5,13 @@ var materialColors = require('material-colors/dist/colors.json');
 module.exports = (function getAvailableColors(fromColors) {
 
   var arr = [];
+  var stipColors = ['white', 'black'];
 
   Object.keys(fromColors).forEach(function (key) {
+
+    if (stipColors.indexOf(key) > -1) {
+      return;
+    }
     Object.keys(fromColors[key]).forEach(function (colorKey) {
       arr.push(fromColors[key][colorKey]);
     });
