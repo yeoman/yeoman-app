d7d9afe9aee4e3e2d11945fab1c5d764d4625fed
Stefan Buck <github@stefanbuck.com>
2015-08-05 19:59:50 +0200

    Fix typo

--- src/renderer/utils/colors.js
+++ src/renderer/utils/colors.js
@@ -5,11 +5,11 @@ var materialColors = require('material-colors/dist/colors.json');
 module.exports = (function getAvailableColors(fromColors) {
 
   var arr = [];
-  var stipColors = ['white', 'black'];
+  var stripColors = ['white', 'black'];
 
   Object.keys(fromColors).forEach(function (key) {
 
-    if (stipColors.indexOf(key) > -1) {
+    if (stripColors.indexOf(key) > -1) {
       return;
     }
     Object.keys(fromColors[key]).forEach(function (colorKey) {
