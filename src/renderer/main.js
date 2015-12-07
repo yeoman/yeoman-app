0b8b4abf5470bb94c6097679cad49c16d25d949a
Long Nguyen <long@seespace.co>
2015-12-07 12:38:34 +0700

    Livereload development with gulp & electron-connect

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -7,15 +7,19 @@ var main = document.createElement('main');
 main.id = 'content';
 document.body.appendChild(main);
 
-var style = document.createElement('link');
-style.rel = 'stylesheet/less';
-style.type = 'text/css';
-style.href = 'less/main.less';
-document.head.appendChild(style);
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+  require('electron-connect').client.create();
+};
 
-var lessCompiler = document.createElement('script');
-lessCompiler.src = '../node_modules/less/dist/less.js';
-document.body.appendChild(lessCompiler);
+// var style = document.createElement('link');
+// style.rel = 'stylesheet/less';
+// style.type = 'text/css';
+// style.href = 'less/main.less';
+// document.head.appendChild(style);
+
+// var lessCompiler = document.createElement('script');
+// lessCompiler.src = '../node_modules/less/dist/less.js';
+// document.body.appendChild(lessCompiler);
 
 // Array.prototype.findIndex polyfill, we can remove this as soon as Chrome starts supporting it
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
