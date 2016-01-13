f102027646e257f05f5653019691f7b537feb8cc
Ruy Adorno <ruyadorno@hotmail.com>
2016-01-12 23:09:44 -0500

    Better generator colors support using mui colors, fixes #62

--- src/renderer/utils/colors.js
+++ src/renderer/utils/colors.js
@@ -1,15 +1,34 @@
-import materialColors from 'material-colors/dist/colors.json';
+import { Styles } from 'material-ui';
 
-let arr = [];
-const stripColors = ['white', 'black'];
+// excludes problematic colors
+const stripColors = [
+  'brown700',
+  'brown800',
+  'brown900',
+  'blueGrey700',
+  'blueGrey800',
+  'blueGrey900',
+  'grey50',
+  'grey100',
+  'grey200',
+  'grey600',
+  'grey700',
+  'grey800',
+  'grey900',
+  'white',
+  'black',
+  'transparent',
+  'fullBlack',
+  'darkBlack',
+  'lightBlack',
+  'minBlack',
+  'faintBlack',
+  'fullWhite',
+  'darkWhite',
+  'lightWhite'
+];
 
-Object.keys(materialColors)
+export default Object.keys(Styles.Colors)
   .filter(color => stripColors.indexOf(color) === -1)
-  .forEach(function (key) {
-    Object.keys(materialColors[key]).forEach((colorKey) => {
-      arr.push(materialColors[key][colorKey]);
-    });
-  });
-
-export default arr;
+  .map(key => Styles.Colors[key]);
 
